47ae32b7345e1da800a4221a4daa30cc9c09a4bf
tt-shin <ttshin03@gmail.com>
2025-01-10 12:27:40 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
 include 'db_config.php';
 
-$id = $_GET['id'];
-$sql = "DELETE FROM users WHERE id=?";
+$userid = $_GET['userid'];
+$sql = "DELETE FROM user WHERE userid=?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $id);
+$stmt->bind_param("i", $userid);
 $stmt->execute();
 $stmt->close();
 header("Location: view.php");
