1c165fd8561e936b340f03f48d7c1603cf999fec
tt-shin <ttshin03@gmail.com>
2025-01-10 12:28:23 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,31 +1,29 @@
 <?php
 include 'db_config.php';
 
-$sql = "SELECT * FROM users";
+$sql = "SELECT * FROM user";
 $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>View Items</title>
+    <title>View User</title>
 </head>
 <body>
     <table border="1">
         <tr>
             <th>ID</th>
             <th>Name</th>
-            <th>Email</th>
             <th>Actions</th>
         </tr>
         <?php while ($row = $result->fetch_assoc()): ?>
             <tr>
-                <td><?= $row['id'] ?></td>
-                <td><?= $row['name'] ?></td>
-                <td><?= $row['email'] ?></td>
+                <td><?= $row['userid'] ?></td>
+                <td><?= $row['username'] ?></td>
                 <td>
-                    <a href="edit.php?id=<?= $row['id'] ?>">Edit</a>
-                    <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+                    <a href="edit.php?id=<?= $row['userid'] ?>">Edit</a>
+                    <a href="delete.php?id=<?= $row['userid'] ?>">Delete</a>
                 </td>
             </tr>
         <?php endwhile; ?>
