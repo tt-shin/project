db3bc28d7aa6593ba01a35c62c865089a739b3d2
tt-shin <ttshin03@gmail.com>
2025-01-10 10:43:07 +0800

    tcc.php

--- tcc.php
+++ tcc.php
@@ -1,3 +1,54 @@
+<?php
+$host = "<AWS_RDS_ENDPOINT>";
+$username = "<DB_USERNAME>";
+$password = "<DB_PASSWORD>";
+$dbname = "<DB_NAME>";
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Add function
+if (isset($_POST['add'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $sql = "INSERT INTO users (name, email) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $name, $email);
+    $stmt->execute();
+    $stmt->close();
+    header("Location: view.php");
+}
+
+// Edit function
+if (isset($_POST['edit'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $sql = "UPDATE users SET name=?, email=? WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssi", $name, $email, $id);
+    $stmt->execute();
+    $stmt->close();
+    header("Location: view.php");
+}
+
+// Delete function
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $sql = "DELETE FROM users WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->close();
+    header("Location: view.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -102,17 +153,17 @@
         <div class="container">
             <div class="menu">
                 <div class="menu-item">
-                    <a href="view.html">
+                    <a href="view.php">
                         <span>View Items</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="add.html">
+                    <a href="add.php">
                         <span>Add Items</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="edit.html">
+                    <a href="edit.php">
                         <span>Edit Items</span>
                     </a>
                 </div>
